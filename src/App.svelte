1a4f174ead70ff308f8338b075a422cf822c9ecc
Justin Moon <mail@justinmoon.com>
2020-02-22 21:43:57 -0600

    Basic replication

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,18 @@
   import { onMount } from 'svelte'
   import PouchDB from 'pouchdb-browser'
 
-  let db = new PouchDB('db')
   let text = ''
   let notes = []
+
+  let db = new PouchDB('db')
+  const replication = PouchDB.sync('db', 'http://localhost:5984/slipbox', {
+    live: true,
+    retry: true
+  }).on('change', async function (info) {
+    await updateNotes()
+  }).on('error', function (err) {
+    console.log('Replication error:', err)
+  })
 
   async function updateNotes() {
     const allDocs = await db.allDocs({
