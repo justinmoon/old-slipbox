32cc480c93ded8b2196a75cad98933ed794eb435
Justin Moon <mail@justinmoon.com>
2020-02-23 01:26:52 -0600

    Backlinks

--- src/routes/Note.svelte
+++ src/routes/Note.svelte
@@ -1,16 +1,26 @@
 <script>
   import { get, derived } from 'svelte/store'
+  import { navigate } from 'svelte-routing'
   export let noteId;
   export let notes
 
   $: note = derived(notes, $notes => $notes.find(note => {
-    console.log(note._id, noteId)
     return note._id === noteId
   }));
 
-  $: console.log('note.svelte note', get(notes))
-  $: console.log('note.svelte note', get(note))
+  $: backLinks = derived(
+    [notes, note],
+    ([$notes, $note]) => $notes.filter(otherNote => otherNote.text.search($note._id) >= 0)
+  )
 </script>
 
 <p>ID: {$note._id}</p>
 <p>Text: {$note.text}</p>
+
+<p>Back Links</p>
+
+<ul>
+  {#each $backLinks as backLink}
+    <li on:click={() => navigate(`/notes/${backLink._id}`)}>{backLink.text}</li>
+  {/each}
+</ul>
