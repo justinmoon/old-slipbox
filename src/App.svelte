f83c7211d804e9977e73c95dcf6fd387c3e8c664
Justin Moon <mail@justinmoon.com>
2020-02-22 21:54:08 -0600

    Routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,56 +1,20 @@
 <script>
-  import { onMount } from 'svelte'
-  import PouchDB from 'pouchdb-browser'
+  import { Router, Route } from "svelte-routing";
+  import NavLink from "./components/NavLink.svelte";
+  import Editor from "./routes/Editor.svelte";
+  import Notes from "./routes/Notes.svelte";
 
-  let text = ''
-  let notes = []
-
-  let db = new PouchDB('db')
-  const replication = PouchDB.sync('db', 'http://localhost:5984/slipbox', {
-    live: true,
-    retry: true
-  }).on('change', async function (info) {
-    await updateNotes()
-  }).on('error', function (err) {
-    console.log('Replication error:', err)
-  })
-
-  async function updateNotes() {
-    const allDocs = await db.allDocs({
-      include_docs: true
-    })
-    notes = allDocs.rows.map(row => row.doc)
-  }
-  async function addNote(event) {
-    const note = {
-      text,
-      createdAt: new Date().toISOString()
-    }
-    const addition = await db.post(note)
-    if (addition.ok) {
-      await updateNotes()
-    }
-    text = ''
-  }
-  onMount(async () => {
-    await updateNotes()
-  })
+  // Used for SSR. A falsy value is ignored by the Router.
+  export let url = "";
 </script>
 
-<main>
-	<p># Notes: {notes.length}</p>
-	<form on:submit|preventDefault={addNote}>
-		<textarea bind:value={text} cols="80" rows="30"></textarea>
-		<button type='submit'>➕ Create Note</button>
-	</form>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		margin: 0 auto;
-	}
-	textarea {
-		margin: 35px 35px 35px 35px;
-	}
-</style>
+<Router url="{url}">
+  <nav>
+    <NavLink to="/">Editor</NavLink>
+    <NavLink to="notes">Notes</NavLink>
+  </nav>
+  <div>
+    <Route path="notes" component="{Notes}" />
+    <Route path="/" component="{Editor}" />
+  </div>
+</Router>
